1e7f5d9b08a8a6519334d6c984e72310b3c13baa
Carlos Zúñiga <carlos.zun@gmail.com>
2021-03-22 10:34:23 -0500

    Simplified the unicode conversion

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,9 @@
 const std = @import("std");
 const user32 = std.os.windows.user32;
+const L = std.unicode.utf8ToUtf16LeStringLiteral;
 
 pub fn main() anyerror!void {
-    //const message = try std.os.windows.sliceToPrefixedFileW("Hello");
-    const message = "Hello";
-    var wide_message: [100:0]u16 = [_:0]u16{0} ** 100;
-    const i = std.unicode.utf8ToUtf16Le(wide_message[0..], message);
-    _ = user32.MessageBoxW(null, wide_message[0..], wide_message[0..], user32.MB_OK | user32.MB_ICONINFORMATION);
+    const message = L("Hello World");
+    const title = L("Hello");
+    _ = user32.MessageBoxW(null, message, title, user32.MB_OK | user32.MB_ICONINFORMATION);
 }
