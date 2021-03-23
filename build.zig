1c4cf27572acc26fa3feaab6bdd4e853750e7aaf
Carlos Zúñiga <carlos.zun@gmail.com>
2021-03-22 19:23:16 -0500

    Added a C implementation for comparison Also commented a translated c to zig version, generate the translated version with:     zig translate-c src/main.c -lc > src/translated.zig

--- build.zig
+++ build.zig
@@ -11,10 +11,10 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
+    // Pure Zig
     const exe = b.addExecutable("handmade", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
-    //exe.linkLibC();
     exe.install();
     exe.linkLibC();
     exe.linkSystemLibrary("gdi32");
@@ -24,7 +24,41 @@ pub fn build(b: *std.build.Builder) void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    // Pure C
+    const c_exe = b.addExecutable("c_handmade", null);
+    c_exe.setTarget(target);
+    c_exe.setBuildMode(mode);
+    c_exe.install();
+    c_exe.linkLibC();
+    c_exe.linkSystemLibrary("gdi32");
+    c_exe.linkSystemLibrary("user32");
+    c_exe.addCSourceFiles(&.{"src/main.c"}, &.{});
+
+    const run_c_cmd = c_exe.run();
+    run_c_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_c_cmd.addArgs(args);
+    }
+    const run_c_step = b.step("runc", "Run the C app");
+    run_c_step.dependOn(&run_c_cmd.step);
+
+    // Translated C to Zig
+    // const t_exe = b.addExecutable("t_handmade", "src/translated.zig");
+    // t_exe.setTarget(target);
+    // t_exe.setBuildMode(mode);
+    // t_exe.install();
+    // t_exe.linkLibC();
+    // t_exe.linkSystemLibrary("gdi32");
+    // t_exe.linkSystemLibrary("user32");
+
+    // const run_t_cmd = t_exe.run();
+    // run_t_cmd.step.dependOn(b.getInstallStep());
+    // if (b.args) |args| {
+    //     run_t_cmd.addArgs(args);
+    // }
+    // const run_t_step = b.step("runt", "Run the translated app");
+    // run_t_step.dependOn(&run_t_cmd.step);
 }
---
+++ src/main.c
@@ -0,0 +1,110 @@
+#include <windows.h>
+#include <stdio.h>
+
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+{
+    LRESULT Result = 0;
+    switch (Message)
+    {
+    case WM_SIZE:
+    {
+        OutputDebugStringA("WM_SIZE\n");
+    }
+    break;
+
+    case WM_ACTIVATEAPP:
+    {
+        OutputDebugStringA("WM_ACTIVATEAPP\n");
+    }
+    break;
+
+    case WM_CLOSE:
+    {
+        OutputDebugStringA("WM_CLOSE\n");
+        exit(0);
+    }
+    break;
+
+    case WM_DESTROY:
+    {
+        OutputDebugStringA("WM_DESTROY\n");
+        exit(0);
+    }
+    break;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT Paint;
+        HDC DeviceContext = BeginPaint(Window, &Paint);
+        LONG X = Paint.rcPaint.left;
+        LONG Y = Paint.rcPaint.top;
+        LONG W = Paint.rcPaint.right - Paint.rcPaint.left;
+        LONG H = Paint.rcPaint.bottom - Paint.rcPaint.top;
+        LONG HalfWidth = W / 2;
+
+        PatBlt(DeviceContext, X, Y, HalfWidth, H, WHITENESS);
+        PatBlt(DeviceContext, X + HalfWidth, Y, HalfWidth, H, BLACKNESS);
+        EndPaint(Window, &Paint);
+    }
+    break;
+
+    default:
+    {
+        // todo
+        Result = DefWindowProcW(Window, Message, WParam, LParam);
+    }
+    break;
+    }
+    return Result;
+}
+
+int CALLBACK WinMain(
+    HINSTANCE Instance,
+    HINSTANCE PrevInstance,
+    LPSTR CommandLine,
+    int ShowCode)
+{
+    puts("Hello world from puts\n");
+    OutputDebugStringA("hello from OutputDebugStringA\n");
+    WNDCLASS WindowClass = {0};
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    WindowClass.lpszClassName = L"HandMadeWindowClass";
+
+    if (RegisterClassA(&WindowClass) == 0)
+    {
+        puts("RegisterClassA == 0\n");
+        OutputDebugStringA("RegisterClassA == 0\n");
+        return 1;
+    }
+    HWND WindowHandle = CreateWindowExA(
+        CS_OWNDC | CS_HREDRAW | CS_VREDRAW,
+        WindowClass.lpszClassName, L"Handmade hero",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        0, 0,
+        Instance, 0);
+    if (WindowHandle == 0)
+    {
+        puts("WindowHandle == 0\n");
+        OutputDebugStringA("WindowHandle == 0\n");
+        return 2;
+    }
+    MSG Message;
+    for (;;)
+    {
+        BOOL MessageResult = GetMessageA(&Message, WindowHandle, 0, 0);
+        if (MessageResult > 0)
+        {
+            TranslateMessage(&Message);
+            DispatchMessage(&Message);
+        }
+        else
+        {
+            puts("MessageResult <= 0\n");
+            OutputDebugStringA("MessageResult <= 0\n");
+            break;
+        }
+    }
+    return 0;
+}
