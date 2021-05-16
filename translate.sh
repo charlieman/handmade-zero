7f78b01aa42085f215b2154236e17fdcdaece31b
Carlos Zúñiga <carlos.zun@gmail.com>
2021-05-15 20:41:33 -0500

    Added script to translate c to zig Because the command has a lot of directory options.

---
+++ translate.sh
@@ -0,0 +1,6 @@
+zig translate-c src/main.c \
+ -I"C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.17763.0\\um" \
+ -I"C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.17763.0\\shared" \
+ -I"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.16.27023\\include" \
+ -I"C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.17763.0\\ucrt" \
+ > src/translated.zig
